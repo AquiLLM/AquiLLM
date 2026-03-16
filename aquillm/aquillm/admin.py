7271c57a7b2e7d691faf9ac4e6a254cd9d728d56
Chandler Campbell <r.chandler.campbell@gmail.com>
2026-03-15 22:21:42 -0600

    Add feedback aggregation export system
    Adds a FeedbackExport model, Celery task, and admin UI that lets admins
    generate JSON exports of all conversations containing rated or
    text-feedback messages. Exports are saved to MinIO and served through a
    Django proxy view to avoid internal-DNS issues with pre-signed S3 URLs.

--- aquillm/aquillm/admin.py
+++ aquillm/aquillm/admin.py
@@ -3,7 +3,9 @@ from django.contrib.contenttypes.admin import GenericTabularInline
 from django.urls import reverse, path
 from django.utils.html import format_html
 from django.shortcuts import render
-from .models import ConversationFile, RawTextDocument, HandwrittenNotesDocument, PDFDocument, VTTDocument, TeXDocument, TextChunk, Collection, CollectionPermission, WSConversation, GeminiAPIUsage
+from django.contrib import messages as django_messages
+from django.http import HttpResponseRedirect, FileResponse, Http404
+from .models import ConversationFile, RawTextDocument, HandwrittenNotesDocument, PDFDocument, VTTDocument, TeXDocument, TextChunk, Collection, CollectionPermission, WSConversation, GeminiAPIUsage, FeedbackExport
 from .ocr_utils import get_gemini_cost_stats
 
 
@@ -94,3 +96,63 @@ class GeminiAPIUsageAdmin(admin.ModelAdmin):
             **self.admin_site.each_context(request),
         }
         return render(request, 'aquillm/gemini_cost_monitor.html', context)
+
+
+@admin.register(FeedbackExport)
+class FeedbackExportAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status', 'created_at', 'completed_at', 'triggered_by',
+                    'conversation_count', 'message_count', 'download_link')
+    list_filter = ('status',)
+    readonly_fields = ('status', 'created_at', 'completed_at', 'triggered_by', 'file',
+                       'conversation_count', 'message_count', 'error_message', 'celery_task_id')
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def download_link(self, obj):
+        if obj.status == 'completed' and obj.file:
+            url = reverse('admin:feedback-export-download', args=[obj.pk])
+            return format_html('<a href="{}">Download JSON</a>', url)
+        return '-'
+    download_link.short_description = 'Download'
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('trigger/', self.admin_site.admin_view(self.trigger_export_view),
+                 name='feedback-export-trigger'),
+            path('<int:export_id>/download/', self.admin_site.admin_view(self.download_export_view),
+                 name='feedback-export-download'),
+        ]
+        return custom_urls + urls
+
+    def download_export_view(self, request, export_id):
+        try:
+            export = FeedbackExport.objects.get(pk=export_id)
+        except FeedbackExport.DoesNotExist:
+            raise Http404
+        if not export.file:
+            raise Http404
+        return FileResponse(export.file.open('rb'), content_type='application/json',
+                            as_attachment=True, filename=export.file.name.split('/')[-1])
+
+    def trigger_export_view(self, request):
+        if request.method == 'POST':
+            export = FeedbackExport.objects.create(triggered_by=request.user)
+            from .feedback_tasks import generate_feedback_export
+            generate_feedback_export.delay(export.pk)
+            django_messages.success(request, f'Feedback export #{export.pk} has been queued.')
+            return HttpResponseRedirect(reverse('admin:aquillm_feedbackexport_changelist'))
+        return render(request, 'aquillm/feedback_export_trigger.html', {
+            'title': 'Generate Feedback Export',
+            'opts': self.model._meta,
+            **self.admin_site.each_context(request),
+        })
+
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['trigger_url'] = reverse('admin:feedback-export-trigger')
+        return super().changelist_view(request, extra_context=extra_context)
